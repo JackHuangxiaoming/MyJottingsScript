79ec5bc8ff61df7ba5fdcd04eb104aef3ffadd9d
Huangxiaomimg <huang19970629@gmail.com>
2017-06-05 13:23:58 +0800

    unity单例类
    如果要继承这个单例类，必须是继承Mono的脚本
    需要传入泛型 泛型为本类对象

---
+++ Single.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+/// <summary>
+/// 优雅的单例
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Single<T> : MonoBehaviour  where T : MonoBehaviour{
+
+    protected static T single;
+
+	public static T Instance
+    {
+        get {
+            if (single == null || single.gameObject == null)
+            {
+                //instance = (T)FindObjectOfType(typeof(T));
+                single = FindObjectOfType<T>();
+                if (single == null)
+                {
+                    Debug.LogWarningFormat("场景中需要一个类型：{0}的实例，但是并没有！",typeof(T));
+                }
+            }
+            return single;
+        }
+    }
+}
